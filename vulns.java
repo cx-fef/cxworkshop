e1c1ab7d9ac06553c155554c866663a87ba0410c
Andrew Mark Feferman <57198537+mfeferman-cx@users.noreply.github.com>
2022-08-25 00:04:39 -0500

    Update vulns.java

--- vulns.java
+++ vulns.java
@@ -25,18 +25,19 @@ public class Vulns {
 			
 
 			// vulnerable sqli
-			/*
+			
 			sql = "select * from users where (email = '" + email + "' and password = '" + password + "')";
 			Statement statement = connection.createStatement();
 			result = statement.executeQuery(sql);
-*/
+			
 			// clean sqli
+			/*
 			sql = "select * from users where email = ? and password = ? ";
 			PreparedStatement ps = connection.prepareStatement(sql);
 			ps.setString(1, email);
 			ps.setString(2, password);
 			result = ps.executeQuery();
-
+			*/
 			if (result.next()) {
 				loggedIn = true;
 				doGet(result,req,response);
