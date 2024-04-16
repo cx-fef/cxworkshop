f0db51ae2b51f2b6270c10a7edf8532bf4fe431c
Mark Feferman <mark.feferman.cx@gmail.com>
2024-04-16 17:43:47 -0500

    changes to java file to include new functions.

--- vulns.java
+++ vulns.java
@@ -3,8 +3,15 @@ import java.sql.DatabaseMetaData;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import javax.servlet.http.HttpServletRequest;
+
 import java.io.PrintWriter;
 import com.google.common.html.HtmlEscapers;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 
 /* feferman */
 
@@ -80,6 +87,9 @@ public class Vulns {
   			String loc = request.getParameter("location");
 			loc+=res.getString("GEO_LOC");
 			
+			logRequest(request);
+			exportToPDF(request);
+			
 			// clean xss
 			/*
  			String escapedLocation = HtmlEscapers.htmlEscaper().escape(loc); 
@@ -95,4 +105,64 @@ public class Vulns {
 			out.println("Error caught by overly broad exception handler: " + ex.Message());
 		}
 	}
+
+	public void logRequest(HttpServletRequest request) {
+		Logger logger = Logger.getLogger("HttpRequestLogger");
+		FileHandler fh;
+	
+		try {
+			// This block configure the logger with handler and formatter
+			fh = new FileHandler("HttpRequestLog.log", true);
+			logger.addHandler(fh);
+			SimpleFormatter formatter = new SimpleFormatter();
+			fh.setFormatter(formatter);
+
+			logger.info("info: " + request.getParameter("location"));
+	
+			// the following statement is used to log any messages
+			logger.info("Request Method: " + request.getMethod());
+			logger.info("Request URI: " + request.getRequestURI());
+			logger.info("Request Protocol: " + request.getProtocol());
+			logger.info("Request Parameters: " + request.getParameterMap().toString());
+	
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// ...
+
+	public void exportToPDF(HttpServletRequest request) {
+		try {
+			// Create a new Document
+			Document document = new Document();
+			
+			// Set the response content type to PDF
+			response.setContentType("application/pdf");
+			
+			// Set the response header for file download
+			response.setHeader("Content-Disposition", "attachment; filename=\"output.pdf\"");
+			
+			// Get the response PrintWriter
+			PrintWriter out = response.getWriter();
+			
+			// Create a new PdfWriter instance
+			PdfWriter.getInstance(document, out);
+			
+			// Open the Document
+			document.open();
+			
+			// Add content to the Document
+			String location = request.getParameter("location");
+			document.add(new Paragraph("Location: " + location));
+			
+			// Close the Document
+			document.close();
+		} catch (DocumentException | IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+
 }
