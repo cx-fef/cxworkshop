f0c060ea157d2433b85c1bc6c88ba7c305f3af86
Mark Feferman <mark.feferman.cx@gmail.com>
2024-05-15 21:33:14 -0500

    fix sqli for this call

--- vulns.java
+++ vulns.java
@@ -1,7 +1,7 @@
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
+//import java.sql.DatabaseMetaData;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+//import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.io.PrintWriter;
 import com.google.common.html.HtmlEscapers;
@@ -9,10 +9,10 @@ import com.google.common.html.HtmlEscapers;
 
 public class Vulns {
 
-	private boolean loggedIn = false;
-	private Result result;
-	private HttpServletResponse response;
-	private HttpServletRequest req;
+	private static boolean loggedIn = false;
+	private static Result result;
+	private static HttpServletResponse response;
+	private static HttpServletRequest req;
 
 	// SQLi vulnerability
 	public static void input (DataSource pool) {
@@ -23,20 +23,18 @@ public class Vulns {
 			String password = request.getParameter ("password");
 			// get a connection to the sql server
 			Connection connection = pool.getConnection();
-			String sql = "";
-			
 
 			// vulnerable SQLi
 /* 
 			// this is what building a sql statement inline is like
-			sql = "select * from users where (email = '" + email + "' and password = '" + password + "')";
+			String sql = "select * from users where (email = '" + email + "' and password = '" + password + "')";
 			Statement statement = connection.createStatement();
 			ResultSet result = statement.executeQuery(sql);
 */
 			// clean sqli
 			// this is the right way to use a preparedstatement, which can be used incorrectly, also. :)
 			
-			sql = "select * from users where email = ? and password = ? ";
+			String sql = "select * from users where email = ? and password = ? ";
 			PreparedStatement ps = connection.prepareStatement(sql);
 			ps.setString(1, email);
 			ps.setString(2, password);
@@ -68,7 +66,7 @@ public class Vulns {
 	}
 	
 	// XSS vulnerability	
-	protected void doGet(Result res, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected static void doGet(Result res, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     
     		try {
 			response.setContentType("text/html;charset=UTF-8");
